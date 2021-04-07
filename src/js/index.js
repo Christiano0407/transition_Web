f644269fd42e2d7690f7ba04bbec6f2e24f56c36
Christiano0407 <placebo0704@gmail.com>
2021-04-07 11:57:56 -0500

    finished-animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
-const t1 = gsap.timeline({paused: true}); 
+const t1 = gsap.timeline({paused: true, default: {duration: .05, ease: `Elastic.easeOut.config(1, 0.3)`}}); 
 const container = document.querySelector(`.container`); 
 
 t1.to(container, {clipPath: `polygon(7% 7%, 93% 7%, 93% 93%, 7% 93%)`})
-t1.to(container, {clipPath: `polygon(93% 7%, 93% 7%, 93% 93%, 93% 93%)`}, `+=1`)
-
+t1.to( `.container2`,{clipPath: `polygon(7% 7%, 93% 7%, 93% 93%, 7% 93%)`}, `+=1`)
+t1.to(container, {clipPath: `polygon(93% 7%, 93% 7%, 93% 93%, 93% 93%)`}, `+=0.5`)
+t1.to(`h1`, {color: `white`})
+t1.to(`.container2`, {clipPath: `polygon(0 0, 100% 0, 100% 100%, 0 100%)` })
 container.addEventListener(`mouseover`, () => {
     t1.play()
 })
